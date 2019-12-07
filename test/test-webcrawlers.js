30eaf11f6e9a39a47035989453ff5bebd4d0c522
Eiji <eijialford@gmail.com>
2019-12-07 15:51:50 -0800

    Added some tests for the web crawler

--- test/test-webcrawlers.js
+++ test/test-webcrawlers.js
@@ -1,19 +1,75 @@
 const chai = require('chai');
-const should = chai.expect;
+const expect = chai.expect;
 const path = require('path');
 const config = require('config');
 process.env.NODE_ENV = 'test';
 
 const muchScrape = require('../backend/crawler/much/muchScrape');
+const muchParse = require('../backend/crawler/much/muchParse');
 
-describe('Testing web crawlers', function(){
+describe('Testing web crawlers for home page for show links', function(){
    it('should webcrawl MUCH website', function(done){
        muchScrape(config.MuchHomeIndex)
            .then(function(links){
-              console.log(links);
               expect(links).to.have.length(6);
               expect(links).to.be.a('array');
               done();
+           });
+   });
+});
+
+describe('Testing that web crawler is getting the correct information', function(){
+   it('should parses South Park episodes', function(done){
+        muchParse(config.MuchShowDir + "south-park/index.html")
+            .then(function(episodes){
+               expect(episodes).to.have.length(3);
+               expect(episodes).to.be.a('array');
+               expect(episodes).to.eql([
+                   {
+                       epi_id: '/shows/south-park/episode/1836304/turd-burglars/',
+                       title: 'South Park',
+                       episode: 'Turd Burglars',
+                       description: 'AIRED NOVEMBER 27, 2019',
+                       link: 'http://www.much.com/shows/south-park/episode/1836304/turd-burglars/'
+                   },
+                   {
+                       epi_id: '/shows/south-park/episode/1824093/board-girls/',
+                       title: 'South Park',
+                       episode: 'Board Girls',
+                       description: 'AIRED NOVEMBER 27, 2019',
+                       link: 'http://www.much.com/shows/south-park/episode/1824093/board-girls/'
+                   },
+                   {
+                       epi_id: '/shows/south-park/episode/1816388/season-finale/',
+                       title: 'South Park',
+                       episode: 'Season Finale',
+                       description: 'AIRED NOVEMBER 27, 2019',
+                       link: 'http://www.much.com/shows/south-park/episode/1816388/season-finale/'
+                   }
+               ]);
+               expect(episodes[0]).to.have.property('title', 'South Park');
+               done();
+            });
+   });
+
+   it('should not parse any episodes in Nathan For You', function(done){
+       muchParse(config.MuchShowDir + "nathan-for-you/index.html")
+           .then(function (episodes) {
+               expect(episodes).to.be.null;
+               done();
+           })
+           .catch(function(err){
+               console.log(err);
+           });
+   });
+
+   it('parses tosh.0 episodes', function(done){
+       muchParse(config.MuchShowDir + "tosh0/index.html")
+           .then(function(episodes){
+               expect(episodes).to.have.length(2);
+               expect(episodes).to.be.a('array');
+               expect(episodes[0]).to.have.property('title', 'Tosh.0');
+               done();
            });
    });
 });
