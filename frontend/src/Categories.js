502683e99b01954b5708a1c2de22bd0b3045bed4
Eiji <eijialford@gmail.com>
2020-02-01 00:40:28 -0800

    Added radio and submit button

--- frontend/src/Categories.js
+++ frontend/src/Categories.js
@@ -5,8 +5,21 @@ export default class Categories extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            genres: ["Comedy", "Animation", "Game-Show"]
-        }
+            genres: ["Action","Adventure","Animation","Biography","Comedy","Crime","Drama","Fantasy","Game-Show"
+                ,"History","Music","Mystery","Reality-TV","Romance","Sci-Fi","Short","Talk-Show"],
+            search: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({search: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        alert(`Searching ${this.state.search}`);
     }
 
     render(){
@@ -18,19 +31,20 @@ export default class Categories extends React.Component{
                     </h3>
                 </div>
                 <div className="jumbotron">
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className="form-group row">
                             {this.state.genres.map(genre =>
                                 <div className="form-check col-3">
-                                    <input className="form-check-input" type="radio" name="categoryRadio" id={genre} value={genre} />
-                                    <label className="form-check-label" for={genre}>
+                                    <label className="form-check-label" htmlFor={genre}>
+                                    <input className="form-check-input" type="radio" name="categoryRadio" id={genre}
+                                           value={genre} checked={this.state.search === genre} onChange={this.handleChange}/>
                                         {genre}
                                     </label>
                                 </div>
                             )}
                         </div>
                         <div className="form-group row">
-                            <div className="col-3">
+                            <div className="col-1">
                                 <button type="submit" className="btn btn-primary">Search</button>
                             </div>
                         </div>
