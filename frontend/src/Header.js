c803138d7e851da1446f3f9800b57fdc74de9584
Eiji <eijialford@gmail.com>
2020-02-08 23:29:19 -0800

    Search is able to work

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,37 +1,6 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 
-// export default function Header(){
-//
-//         return(
-//             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//                 <div className="container">
-//                     <Link className="navbar-brand" to="/">TV Poop</Link>
-//                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-//                             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                         <span className="navbar-toggler-icon"></span>
-//                     </button>
-//                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                         <ul className="navbar-nav mr-auto">
-//                             <li className="nav-item my-2">
-//                                 <Link className="nav-link" to="/new_release">New Releases<span className="sr-only">(current)</span></Link>
-//                             </li>
-//                             <li className="nav-item my-2">
-//                                 <Link className="nav-link" to="/networks">Networks</Link>
-//                             </li>
-//                             <li className="nav-link">
-//                                 <Link className="nav-link" to="/categories">Categories</Link>
-//                             </li>
-//                         </ul>
-//                         <form className="form-inline my-2 my-lg-0">
-//                             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-//                             <button className="btn btn-outline-light my-2 my-sm-0" type="Submit">Search</button>
-//                         </form>
-//                     </div> {/* navbar */}
-//                 </div> {/* container */}
-//             </nav>
-//         );
-// }
 
 export default class Header extends React.Component{
     constructor(props) {
@@ -47,8 +16,8 @@ export default class Header extends React.Component{
         this.setState({search: event.target.value});
     }
 
-    handleSubmit(event){
-        event.preventDefault();
+    handleSubmit(){
+        return "/search/" + this.state.search;
     }
 
     render(){
@@ -74,7 +43,9 @@ export default class Header extends React.Component{
                         </ul>
                         <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
                             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.handleChange}/>
+                            <Link to={this.handleSubmit}>
                             <button className="btn btn-outline-light my-2 my-sm-0" type="Submit">Search</button>
+                            </Link>
                         </form>
                     </div> {/* navbar */}
                 </div> {/* container */}
